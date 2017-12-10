4b297c4f3b37aa2b7069cb4fab6ce70e6e9df580
Carlos Camilo Lobo Ulloque <camilo@MacBook-Pro-de-Carlos.local>
2017-12-09 21:18:27 -0500

    Added npm library to install dependencies.

--- src/tasks/generateProject.js
+++ src/tasks/generateProject.js
@@ -4,19 +4,38 @@ import { sync } from 'mkpath';
 import chalk from 'chalk';
 import { exec } from 'child_process';
 import animate from 'chalk-animation';
+import npm from 'npm-programmatic';
 
 const PROJECT_TEMPLATE_PATH = resolve(__dirname, '../../templates/project');
 let INSTALL_DEPS_MESSAGE = 'Installing dependencies';
 const INSTALL_DEPS_ANIMATION = animate.glitch(`${chalk.yellowBright(INSTALL_DEPS_MESSAGE)}`, 0.3);
 
-const npmInstall = (projectPath, animationInterval) =>
-	exec(`cd ${projectPath} && npm install`, (err, stdout, stderr) => {
-		if (err) throw err;
+const DEPS = [ 'babel-plugin-transform-runtime', 'babel-polyfill', 'babel-runtime', 'bz-util', 'regenerator-runtime' ];
+const DEV_DEPS = [
+	'babel-cli',
+	'babel-core',
+	'babel-plugin-transform-object-rest-spread',
+	'babel-preset-es2015',
+	'babel-preset-stage-3',
+	'bz-define',
+	'copyfiles',
+	'cpr',
+	'rimraf'
+];
 
-		INSTALL_DEPS_ANIMATION.stop();
-		clearInterval(animationInterval);
-		console.log(`${chalk.greenBright(`Your project is ready! Happy coding <3`)}`);
-	});
+const npmInstall = (projectPath, animationInterval) =>
+	Promise.all([
+		npm.install(DEPS, { cwd: projectPath, save: true }),
+		npm.install(DEV_DEPS, { cwd: projectPath, saveDev: true })
+	])
+		.then(() => {
+			INSTALL_DEPS_ANIMATION.stop();
+			clearInterval(animationInterval);
+			console.log(`${chalk.greenBright(`Your project is ready! Happy coding <3`)}`);
+		})
+		.catch((err) => {
+			throw err;
+		});
 
 const addDots = () =>
 	setInterval(() => {
