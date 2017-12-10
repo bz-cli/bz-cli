779ccf9f347831dd13f1a93d280d24e953c1ffcf
Carlos Camilo Lobo Ulloque <camilo@MacBook-Pro-de-Carlos.local>
2017-12-09 21:11:22 -0500

    Fixed weird typos.

--- src/tasks/generateProject.js
+++ src/tasks/generateProject.js
@@ -7,16 +7,21 @@ import animate from 'chalk-animation';
 
 const PROJECT_TEMPLATE_PATH = resolve(__dirname, '../../templates/project');
 let INSTALL_DEPS_MESSAGE = 'Installing dependencies';
-const INSTALL_DEPS_ANIMATION = animate.glitch(`${chalk.yellowBright(str)}`, 0.3);
+const INSTALL_DEPS_ANIMATION = animate.glitch(`${chalk.yellowBright(INSTALL_DEPS_MESSAGE)}`, 0.3);
 
 const npmInstall = (projectPath, animationInterval) =>
 	exec(`cd ${projectPath} && npm install`, (err, stdout, stderr) => {
 		if (err) throw err;
 
-    INSTALL_DEPS_MESSAGE.stop();
+		INSTALL_DEPS_ANIMATION.stop();
 		clearInterval(animationInterval);
 		console.log(`${chalk.greenBright(`Your project is ready! Happy coding <3`)}`);
 	});
+
+const addDots = () =>
+	setInterval(() => {
+		INSTALL_DEPS_ANIMATION.replace((INSTALL_DEPS_MESSAGE += '.'));
+	}, 300);
 
 export const generateProject = (appName) => {
 	const PROJECT_PATH = `${process.cwd()}/${appName}`;
@@ -26,14 +31,10 @@ export const generateProject = (appName) => {
 	ncp(PROJECT_TEMPLATE_PATH, appName, (err) => {
 		if (err) throw err;
 
-    console.log(`${chalk.greenBright('create')} ${chalk.gray(PROJECT_PATH)}`);
+		console.log(`${chalk.greenBright('create')} ${chalk.gray(PROJECT_PATH)}`);
 		console.log(`${chalk.gray('Please wait a while. ')}`);
-    INSTALL_DEPS_ANIMATION.start();
+		INSTALL_DEPS_ANIMATION.start();
 
 		npmInstall(PROJECT_PATH, addDots());
 	});
 };
-
-const addDots = setInterval(() => {
-  INSTALL_DEPS_ANIMATION.replace((INSTALL_DEPS_MESSAGE += '.'));
-}, 300);
