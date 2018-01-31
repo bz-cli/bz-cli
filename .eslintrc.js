ad2da3e774b6c8adb4a91070f28b733d66d702f8
Carlos Camilo Lobo Ulloque <carloscamilo.loboulloque@gmail.com>
2018-01-31 08:58:09 -0500

    Added linter and updated gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'comma-dangle': 'off',
+    'no-console': 'off'
+  }
+};
