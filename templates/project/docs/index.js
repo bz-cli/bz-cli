4ca74171c24bdfd93a49b34d09f53cdd05e5345c
Carlos Camilo Lobo Ulloque <carloscamilo.loboulloque@gmail.com>
2018-01-31 13:54:25 -0500

    Typo in env variable.

--- templates/project/docs/index.js
+++ templates/project/docs/index.js
@@ -1,7 +1,7 @@
 const generateEnglishDocs = require('./generate-english-docs');
 const generateSpanishDocs = require('./generate-spanish-docs');
 
-if (process.env.lang.toLowerCase() === 'en') {
+if (process.env.lan.toLowerCase() === 'en') {
   generateEnglishDocs();
 } else if (process.env.lang.toLowerCase() === 'es') {
   generateSpanishDocs();
