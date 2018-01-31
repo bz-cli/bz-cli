db2ce9d273d99a785512cc45540b0c2a7253569e
Carlos Camilo Lobo Ulloque <carloscamilo.loboulloque@gmail.com>
2018-01-31 14:33:09 -0500

    Fixed typo in docs generator.

--- templates/project/docs/index.js
+++ templates/project/docs/index.js
@@ -3,6 +3,6 @@ const generateSpanishDocs = require('./generate-spanish-docs');
 
 if (process.env.lan.toLowerCase() === 'en') {
   generateEnglishDocs();
-} else if (process.env.lang.toLowerCase() === 'es') {
+} else if (process.env.lan.toLowerCase() === 'es') {
   generateSpanishDocs();
 }
